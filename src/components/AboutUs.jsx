4be3d1137a4c0c555c30d0b1461f978459e953bc
AleksaSantrac <aleksa123@tutanota.com>
2023-05-09 01:56:19 +0200

    Nova stranica

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,18 @@
+const AboutUs = () => {
+    return ( 
+        <div className="about-us">
+            <h1>What should i buy?</h1>
+            <br />
+            <div className="text">
+                Now that you know how much you can comfortably spend, it’s time to think about what type of car is right for you. There are different ways to do this, but one of the most effective methods is to consider how your new vehicle will be used and the needs it’ll have to meet. Do you have a few kids and need room for them and all their stuff? Are you looking for a car that’s reasonably comfortable and efficient for the daily commute? Or do you need a vehicle that can comfortably tow a camper? 
+
+                Reading expert car reviews and watching video reviews and virtual test drives is a great way to learn about a car from the comfort of your couch, and Cars.com’s multicar comparison tests will show you how a whole class of vehicles, like compact SUVs, stack up against each other. Consumer reviews can offer additional insights, too. If you’re more interested in features-for-the-money, a specs comparison will show you multiple models’ features, side by side. 
+
+                Once you have a handful of possible models there’s one more step to take — talking to your insurance company — before starting your car search. This is important so you know how much each car you’re considering would cost to insure, as monthly premiums can vary considerably. 
+            </div>
+            
+        </div>
+     );
+}
+ 
+export default AboutUs;
